33bb1bdcbba6d7402b0305d19ac3869b5a4aeec7
sarbajitdutta <sarbajitdutta35@gmail.com>
2018-08-22 16:39:13 -0400

    added pubmedPubdate and history

--- src/reciter/model/pubmed/History.java
+++ src/reciter/model/pubmed/History.java
@@ -1,19 +1,23 @@
 package reciter.model.pubmed;
 
+import java.util.List;
+
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
 import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Builder
 @Getter
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @DynamoDBDocument
 @NoArgsConstructor
-public class PubMedData {
-	private History history;
-	private String publicationStatus;
-    private ArticleIdList articleIdList;
-    
+@AllArgsConstructor
+public class History {
+	private List<PubMedPubDate> pubmedPubDate;
 }
